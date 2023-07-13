417e43c002f302ce5dffc13562964c7d43467f4e
rajeshbhr <rajeshbehera69698@gmail.com>
2023-07-13 22:41:36 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/bharat_quiz_task2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bharat_quiz_task2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/result_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../ui/shared/color.dart';
+import 'main_menu.dart';
+
+
+class ResultScreen extends StatefulWidget {
+  int score;
+  ResultScreen(this.score, {Key? key}) : super(key: key);
+
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.pripmaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Text(
+              "Congratulations",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 45.0,
+          ),
+          Text(
+            "You Score is",
+            style: TextStyle(color: Colors.white, fontSize: 34.0),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "${widget.score}",
+            style: TextStyle(
+              color: Colors.orange,
+              fontSize: 85.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 100.0,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MainMenu(),
+                  ));
+            },
+            style: ButtonStyle(padding: MaterialStatePropertyAll(EdgeInsets.all(18.0),),
+              backgroundColor: MaterialStatePropertyAll( AppColor.secondaryColor),
+              shape: MaterialStatePropertyAll(StadiumBorder(),),),
+
+            child: Text(
+              "Reapeat the quizz",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
